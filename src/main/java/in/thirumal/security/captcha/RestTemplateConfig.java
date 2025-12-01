7ed0c0865ea9075d560f1c03d548bfeb21169d4b
Thirumal <m.thirumal@hotmail.com>
2025-12-01 20:31:25 +0530

    version downgrade

--- src/main/java/in/thirumal/security/captcha/RestTemplateConfig.java
+++ src/main/java/in/thirumal/security/captcha/RestTemplateConfig.java
@@ -15,18 +15,18 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
-    /*
-     * Bean is required for re-captcha
-     */
-    @Bean
-    RestTemplate restTemplate() {
-	    return new RestTemplate(httpRequestFactory());
+	/*
+	 * Bean is required for re-captcha
+	 */
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate(httpRequestFactory());
 	}
 
 	private ClientHttpRequestFactory httpRequestFactory() {
-		 HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-	     factory.setConnectTimeout(99999);
-	     return factory;
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		factory.setConnectionRequestTimeout(99999);
+		return factory;
 	}
 
 }
